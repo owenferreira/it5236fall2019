fc55a15c9bc6ad76b4e6b65a715b30a1a86bd17d
EC2 Default User <ec2-user@ip-172-31-27-242.us-east-2.compute.internal>
2019-10-23 22:20:50 +0000

    fixed api

--- api/task.php
+++ api/task.php
@@ -1,19 +1,6 @@
 <?php
-
-
-
-ini_set('display_errors', 1);
-
-ini_set('display_startup_errors', 1);
-
-error_reporting(E_ALL);
-
-
-
-    // Declare the credentials to the database
-
+// Declare the credentials to the database
 $dbconnecterror = false;
-
 $dbh = NULL;
 require_once '/var/www/html/credentials.php';
 try{
@@ -32,8 +19,8 @@ try{
     echo "Database timeout";
     exit();
 }
-//Update a task
 
+//Update a task
 if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 
 	if(array_key_exists('listID', $_GET)){
@@ -118,26 +105,21 @@ if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 
 //make a task
 else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
-
     //Decoding the json body from the request.
 
     $task = json_decode(file_get_contents('php://input'), true);
     //IF the task == null, then exit, because there is nothing to POST
-
     if ($task === null) {
-
         http_response_code(400);
+		echo 'no json body';
         exit(); //no data in body.
-
     }
 
     if (array_key_exists('completed', $task)) {
-
         $complete = $task["completed"];
 	} else {
         http_response_code(400);
-
+		echo "no completion status";
         exit();
 
 	}
@@ -146,16 +128,14 @@ else if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $taskName = $task["taskName"];
     } else {
         http_response_code(400);
+		echo "no task name";
         exit();
     }
     if (array_key_exists('taskDate', $task)) {
-
         $taskDate = $task["taskDate"];
-
     } else {
-
         http_response_code(400);
-
+		echo "no task date";
         exit();
     }
 
@@ -172,8 +152,9 @@ else if ($_SERVER['REQUEST_METHOD'] == "POST") {
 			$stmt->bindParam(":listItem", $taskName);
 			$stmt->bindParam(":finishDate", $taskDate);
 			$response = $stmt->execute();
-                http_response_code(204);
-                exit();
+            http_response_code(201);
+            exit();
+			
 		} catch (PDOException $e) {
             http_response_code(504);
 
@@ -194,78 +175,34 @@ else if ($_SERVER['REQUEST_METHOD'] == "POST") {
 else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
 
 	if(array_key_exists('listID', $_GET)){
-
 		$listID = $_GET['listID'];
-
-	}else {
-
-        http_response_code(504);
-
-        exit();
-	}
-    //Decoding the json body from the request.
-
-    $task = json_decode(file_get_contents('php://input'), true);
-    //IF the task == null, then exit, because there is nothing to POST
-
-    if ($task === null) {
-
-        http_response_code(400);
-        exit(); //no data in body.
-
-    }
-    if (array_key_exists('deleteTask', $task)) {
-
-        $deleteTask = $task["deleteTask"];
 	} else {
-        http_response_code(400);
-
+        http_response_code(504);
         exit();
-
 	}
-
-  if ($deleteTask == 1){
+    
     if (!$dbconnecterror) {
-
       try {
+		  $sql = "DELETE FROM doList where listID = :listID";
 
-        $sql = "DELETE FROM doList where listID = :listID";
-
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(":listID", $listID);
-        $response = $stmt->execute();
-                  http_response_code(204);
-                  exit();
+			$stmt = $dbh->prepare($sql);
+			$stmt->bindParam(":listID", $listID);
+			$response = $stmt->execute();
+		  http_response_code(204);
+		  exit();
+		  
       } catch (PDOException $e) {
-              http_response_code(505);
-
-              echo "error running query";
-
-              exit();
-      }
-    } else {
-
-          http_response_code(504);
-          echo "db connect error";
-      exit();
-
-  }
-  } else {
-    http_response_code(504);
-    echo "deleteTask has to be 1 to delete";
-    exit();
-  }
-
-}
-
-else {
-
+		  http_response_code(505);
+		  echo "error running query";
+		  exit();
+		}
+	} else {
+		http_response_code(505);
+		echo "error running query";
+		exit();
+	}
+} else {
     http_response_code(405);//method not allowed
-
     echo "expected put or delete or post request";
-
     exit();
-
 }
-
-?>
--- api/tasks.php
+++ api/tasks.php
@@ -1,21 +1,7 @@
 <?php
-
-
-
-ini_set('display_errors', 1);
-
-ini_set('display_startup_errors', 1);
-
-error_reporting(E_ALL);
-
-
-
-    // Declare the credentials to the database
-
-$dbconnecterror = false;
-
+// Declare the credentials to the database
 $dbh = NULL;
-require_once '/var/www/html/credentials.php';
+require_once 'credentials.php';
 try{
 
     $conn_string = "mysql:host=".$dbserver.";dbname=".$db;
@@ -26,61 +12,30 @@ try{
 
 }catch(Exception $e){
 	//Database issues were encountered.
-
-    //$dbconnecterror = TRUE;
     http_response_code(504);
     echo "Database timeout";
     exit();
 }
-//Update a task
 
+// get all tasks
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
+	try {
+		$sql = "SELECT * FROM doList";
 
-	if(array_key_exists('listID', $_GET)){
-
-		$listID = $_GET['listID'];
-
-	}else {
-
-        http_response_code(504);
-
-        exit();
+		$stmt = $dbh->prepare($sql);
+		$response = $stmt->execute();
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		http_response_code(200);
+		echo json_encode($result);
+		exit();
+	} catch (PDOException $e) {
+		http_response_code(504);
+		echo "error running query";
+		exit();
 	}
-	if (!$dbconnecterror) {
-
-		try {
-
-			$sql = "SELECT * FROM doList WHERE listID=:listID";
-
-			$stmt = $dbh->prepare($sql);
-
-			$stmt->bindParam(":listID", $listID);
-			$response = $stmt->execute();
-                http_response_code(204);
-                exit();
-		} catch (PDOException $e) {
-            http_response_code(504);
-
-            echo "error running query";
-
-            exit();
-		}
-	} else {
-
-        http_response_code(504);
-        echo "db connect error";
-    exit();
-
-}
-}
-else {
-
+} else {
     http_response_code(405);//method not allowed
-
     echo "expected get request";
-
     exit();
-
 }
-
-?>
